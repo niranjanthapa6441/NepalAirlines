58adef868cc94fcc14285be0555439f8668a3abb
niranjanthapa6441 <niranjanthapa644122@gmail.com>
2022-04-03 06:34:17 +0545

    first commit

---
+++ src/main/java/com/FYP/NepalAirlines/NepalAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.FYP.NepalAirlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepalAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepalAirlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FYP/NepalAirlines/NepalAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FYP.NepalAirlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepalAirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
